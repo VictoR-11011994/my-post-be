7ab311f9831cbdffaefe94d762d22db2d33fcecd
VictoR-11011994 <victor.carablut@gmail.com>
2023-02-13 11:31:39 +0100

    first commit

---
+++ src/main/java/com/victorcarablut/code/MyPostBeApplication.java
@@ -0,0 +1,13 @@
+package com.victorcarablut.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPostBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPostBeApplication.class, args);
+	}
+
+}
